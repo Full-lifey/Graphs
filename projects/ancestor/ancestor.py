fece54c8c7b37caed40c2a1cf3a43abb0d5d2252
Joel Perez <joelness03@gmail.com>
2019-11-05 12:37:58 -0700

    ancestor ajacency list created

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,17 @@
 
 def earliest_ancestor(ancestors, starting_node):
-    pass
+    # build an ajacency list from ancestors, keys are children, values are parents
+    # create blank list
+    ancestor_list = {}
+    # for loop over ancestors
+    for node in ancestors:
+        print(node)
+        # if child key exists
+        if node[1] in ancestor_list.keys():
+            # add parent to existing set
+            ancestor_list[node[1]].add(node[0])
+        # else, create key with parent in value set
+        else:
+            ancestor_list[node[1]] = {node[0]}
+
+    # traverse child to parents until None
